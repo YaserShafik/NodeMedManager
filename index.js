3f26d6d9311a625d1c492be104388d9155e0c0cb
UEBERO <contactoyaser@gmail.com>
2024-10-10 06:31:02 +0200

    Solved EADDRINUSE error

--- index.js
+++ index.js
@@ -83,7 +83,3 @@ mongoose.connect(process.env.MONGO_URI, {
   process.exit(1); 
 });
 
-
-// Init server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
